5bba38a734c402fa2868476a1984cd977b6bfb4f
wwwu <965687176@qq.com>
2021-01-26 00:12:41 +0800

    update001

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,106 @@
 <!--  -->
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control">
+
+    </tab-control>
+    <ul>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar.vue';
+import TabControl from 'components/content/tabControl/TabControl.vue'
+
+import {getHomeMultidata} from 'network/home.js';
+
+import HomeSwiper from './childComps/HomeSwiper';
+import RecommendView from './childComps/RecommendView';
+import FeatureView from './childComps/FeatureView';
+
 export default {
   data () {
     return {
+      banners : [],
+      recommends : [],
     }
+  },
+  created(){
+    getHomeMultidata().then(res=>{
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  },
+  components:{
+    NavBar,
+    TabControl,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    
   }
 }
 </script>
 
 <style  scoped>
-
+#home{
+  padding-top: 44px;
+}
+.home-nav{
+  background-color: var(--color-tint);
+  color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control{
+  position:sticky;
+  top:44px;  
+}
 </style>
